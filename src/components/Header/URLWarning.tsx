df3cbc90e6747a99f991ac9b2721ba5d639616d6
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-01-27 22:09:29 -0700

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -29,17 +29,16 @@ export default function URLWarning() {
   return isMobile ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
-        <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.uniswap.org</code>
+        <AlertTriangle style={{ marginRight: 6 }} size={12} /> DON'T GET YOUR SHIT jacked
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.VIKSWAP.org</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
   ) : window.location.hostname === 'app.uniswap.org' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
-        <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.uniswap.org</code> - bookmark it
-        to be safe.
+        <AlertTriangle style={{ marginRight: 6 }} size={12} /> Yo, :-)  Make sure you're on 
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.VIKSWAP.org</code> - OR YOUR SHIT WILL GET JACKED HOMEY!.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
